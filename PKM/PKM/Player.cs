04ccaeaaf03e9128f5450bee129c0e1dac3ad34d
Dario <otrantodario@gmail.com>
2021-07-06 19:16:04 -0300

    Progreso General

---
+++ PKM/PKM/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Player
+{
+    Pokemon[] PokmTeam = new Pokemon [6];
+    public Player() 
+    {
+        Pokemon Charizard = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Charizard.PCM");
+        Pokemon Dragonite = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Dragonite.PCM");
+        Pokemon Ninetails = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Ninetails.PCM");
+        Pokemon Pikachu = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Pikachu.PCM");
+        Pokemon Quilava = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Quilava.PCM");
+        Pokemon Snorlax = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Snorlax.PCM");
+
+        PokmTeam{ Charizard}
+    }
+
+}
+
---
+++ PKM/PKM/TrainerIA.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class TrainerIA
+{
+    public TrainerIA()
+    {
+        currentstates = STATES.WAIT;
+    }
+    private enum STATES
+    {
+        WAIT,
+        ATTK,
+        DEFF,
+        HEAL,
+        SWAPPKM,
+        ERROR
+    }
+    private STATES currentstates;
+    public void Update()
+    {
+        switch (currentstates)
+        {
+            case STATES.WAIT:
+                WAIT();
+                if (PKMCANFIGHT())
+                {
+                    currentstates = STATES.ATTK;
+                }
+                else
+                {
+                    currentstates = STATES.DEFF;
+                }
+                break;
+            case STATES.ATTK:
+                ATTK();
+                break;
+            case STATES.DEFF:
+                DEFF();
+                break;
+            case STATES.HEAL:
+                HEAL();
+                break;
+            case STATES.SWAPPKM:
+                SWAPPKM();
+                break;
+            default:
+                Console.WriteLine("ERROR!!");
+                break;
+        }
+    }
+    private void ATTK() { }
+    private void DEFF() { }
+    private void HEAL() { }
+    private void SWAPPKM() { }
+    private void WAIT() { }
+    private bool PKMCANFIGHT() 
+    {
+        
+        
+        
+        
+        
+        
+            return true;
+        
+       
+    }
+}
+
+
