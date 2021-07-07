e6898832ddb7376febde989a900e1bafb38518df
Dario <otrantodario@gmail.com>
2021-07-06 22:35:42 -0300

    player added

--- PKM/PKM/Player.cs
+++ PKM/PKM/Player.cs
@@ -8,17 +8,21 @@ using System.Threading.Tasks;
 
 class Player
 {
-    Pokemon[] PokmTeam = new Pokemon [6];
+    List<Pokemon> PokmTeam = new List<Pokemon>();
     public Player() 
     {
         Pokemon Charizard = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Charizard.PCM");
+        PokmTeam.Add(Charizard);
         Pokemon Dragonite = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Dragonite.PCM");
+        PokmTeam.Add(Dragonite);
         Pokemon Ninetails = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Ninetails.PCM");
+        PokmTeam.Add(Ninetails);
         Pokemon Pikachu = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Pikachu.PCM");
+        PokmTeam.Add(Pikachu);
         Pokemon Quilava = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Quilava.PCM");
+        PokmTeam.Add(Quilava);
         Pokemon Snorlax = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Snorlax.PCM");
-
-        PokmTeam{ Charizard}
+        PokmTeam.Add(Snorlax);
     }
 
 }
