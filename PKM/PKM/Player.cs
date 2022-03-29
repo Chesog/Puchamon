7d08e252b598112785a568290bb54dda4cbc86e5
Dario <otrantodario@gmail.com>
2022-03-29 19:13:17 -0300

    cambie el player

--- PKM/PKM/Player.cs
+++ PKM/PKM/Player.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 
 
 class Player
 {
     List<Pokemon> PokmTeam = new List<Pokemon>();
+    string Playername;
     public Player() 
     {
+        string[] lines;
+        lines = File.ReadAllLines("./PlayerPkmG/Charizard.PCM");
+        this.Playername = lines[0];
+
         Pokemon Charizard = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Charizard.PCM");
         PokmTeam.Add(Charizard);
         Pokemon Dragonite = PokemonReader.ReadPokemonFromFile("./PlayerPkmG/Dragonite.PCM");
